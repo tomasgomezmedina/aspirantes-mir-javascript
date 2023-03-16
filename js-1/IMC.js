949512d8f2baf6d18749fd3e605157f9382051fe
tomasgomezmedina <rembej544@gmail.com>
2023-03-16 16:20:17 -0500

    correciones

--- js-1/IMC.js
+++ js-1/IMC.js
@@ -1,10 +1,15 @@
 function bmi(peso, altura) {
     if (peso < 0 || altura < 0) {
-        console.log("Los valores no pueden ser negativos");
-    } else if (typeof peso != "number" || typeof altura != "number") {
+        return "Los valores no pueden ser negativos";
+    }
+    else if (typeof peso != "number" || typeof altura != "number") {
         return "Los valores deben ser numeros";
+
     } else {
         return peso / (altura ** 2);
     }
 }
-console.log("El IMC es: " + bmi(75, 1.75));
+// código de prueba
+console.log(bmi(65, 1.8)) // 20.061728395061728
+console.log(bmi(72, 1.6)) // 28.124999999999993
+console.log(bmi(52, 1.75)) //  16.979591836734695
--- js-1/sum.js
+++ js-1/sum.js
@@ -1,4 +1,7 @@
 function suma(num) {
+    if (num < 0) {
+        return "El numero debe ser positivo";
+    }
     let acum=0;
     for (let i = 0; i < num+1; i++) {
         acum += i;
